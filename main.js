00092f29cd4768060d39a5046a82dbf7f9856e46
awadh <awadh.hassan@student.moringaschool.com>
2022-08-29 00:49:44 +0300

    assignment done

--- main.js
+++ main.js
@@ -1,8 +1,34 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+document.getElementById("modal").classList.add("hidden");
 
 // Your JavaScript code goes here!
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded and parsed');
+  document.getElementById("modal").classList.add("hidden");
+  
+    document.getElementsByClassName("like-glyph")[0].onclick = function (){
+    document.getElementsByClassName("like-glyph")[0].classList.toggle("activated-heart")
+
+    mimicServerCall().then(resp => {
+      document.getElementsByClassName("like-glyph")[0].classList.toggle("activated-heart")
+
+
+    }) .catch(err =>{
+      document.getElementById("modal").classList.remove("hidden");
+      setTimeout(function(){ 
+        document.getElementById("modal").classList.add("hidden")
+      } ,3000)
+    })
+
+    
+    
+  }
+  
+});
 
 
 
